0202378236392dc95c720e9e93d496175ffb0d5f
LG-CPFL <162667285+LG-CPFL@users.noreply.github.com>
2024-03-15 17:13:02 +1030

    adding tsc

--- README.md
+++ README.md
@@ -3,10 +3,4 @@ a company office add-in.
 for what purpose?
 currently unsure.
 ## wip
-- add to office as addin (will only do anything online i think?)
-    - go to admin centre
-    - settings > integrated apps > deployed apps > upload custom app
-    - app type: office add-in
-    - point to manifest xml
-- test a lot
-- add css file?
+- figure out how to do this in typescript
---
+++ src/taskpane.ts
@@ -0,0 +1,27 @@
+// define taskpane html and elements
+const html = document;
+const runButton = html.getElementById("runButton");
+const textField = html.getElementById("textField");
+
+// check that office is ready
+Office.onReady( () => {
+    // check that the app is loaded
+    html.onReady( () => {
+        // when button is clicked
+        runButton.addEventListener("click", function () {
+            Word.run(main) // run main function
+            .catch(log => console.error(log)) // unless it breaks
+        });
+    });
+});
+  
+// script goes here (testing testing)
+async function main(context) {
+    const document = context.document;
+    
+    textField.onload(
+        document.body.insertParagraph("Hello " + textField.value, "End")
+    );
+    await context.sync();
+
+}
