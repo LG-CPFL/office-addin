2cce302f0d372706396f546b28552c66caa4d889
LG-CPFL <162667285+LG-CPFL@users.noreply.github.com>
2024-03-19 14:18:51 +1030

    fiddled with compile path in package and added exits to compiler

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,5 @@
+# compilation script run through package.json
+
 if tsc; then
     # If TypeScript compilation is successful, check if there are changes to commit
     if [ -n "$(git status --porcelain)" ]; then
@@ -8,8 +10,10 @@ if tsc; then
     else
         # If there are no changes, print a message
         echo "nothing to commit"
+        exit 0
     fi
 else
     # If TypeScript compilation fails, print error details
     echo "compilation failed"
+    exit 1
 fi
