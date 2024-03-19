757606648e60484e91937ea2d009ce2034512f22
LG-CPFL <lg@clarkpanagakos.com.au>
2024-03-19 01:44:29 +0000

    compiled typescript

--- dist/taskpane.js
+++ dist/taskpane.js
@@ -1,27 +1,30 @@
-// define taskpane html and elements
+"use strict";
+/* import dependencies - not sure any of this is necessary
+/// <reference types="office-js" />
+import * as Office from 'office.js';
+*/
+// global declarations
 const html = document;
 const runButton = html.getElementById("runButton");
 const textField = html.getElementById("textField");
-
 // check that office is ready
-Office.onReady( () => {
-    // check that the app is loaded
-    html.onReady( () => {
-        // when button is clicked
-        runButton.addEventListener("click", function () {
+Office.onReady(() => {
+    // check that the html has loaded
+    html.addEventListener("DOMContentLoaded", () => {
+        runButton.addEventListener("click", () => {
             Word.run(main) // run main function
-            .catch(log => console.error(log)) // unless it breaks
+                .catch((log) => console.error(log)); // unless it breaks
         });
     });
 });
-  
-// script goes here
+// run button function goes here
 async function main(context) {
-    const document = context.document;
-    
-    textField.onload(
-        document.body.insertParagraph("Hello " + textField.value, "End")
-    );
+    const content = context.document.body;
+    if (textField.value === "") {
+        content.insertParagraph("Who goes there?", "End");
+    }
+    else {
+        content.insertParagraph("Hello " + textField.value, "End");
+    }
     await context.sync();
-
 }
