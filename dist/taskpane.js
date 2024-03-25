c10f6e1a6ba6f814815d44aa06852bfafffeba35
LG-CPFL <lg@clarkpanagakos.com.au>
2024-03-25 06:16:38 +0000

    compiled typescript

--- dist/taskpane.js
+++ dist/taskpane.js
@@ -3,18 +3,34 @@
 const html = document;
 const runButton = html.getElementById("runButton");
 const textField = html.getElementById("textField");
-Office.onReady(() => {
-    // check that the html has loaded
-    html.addEventListener("DOMContentLoaded", () => {
-        // when the button is clicked
-        runButton.addEventListener("click", () => {
-            Word.run(main) // run the main function
-                .catch((errorMessage) => console.error(errorMessage)); // unless it breaks
-        });
+// initialise application
+async () => {
+    await Office.onReady(info => {
+        if (info.host === Office.HostType.Word) {
+            html.addEventListener("DOMContentLoaded", () => {
+                attempt(main);
+            });
+        }
     });
-});
-// runButton function
-async function main(context) {
+};
+// error handling
+async function attempt(fn) {
+    try {
+        await fn();
+    }
+    catch {
+        (lg) => console.error(lg);
+    }
+}
+// event triggers
+async function main() {
+    // click the run button
+    runButton.onclick = async () => {
+        await Word.run(runScript);
+    };
+}
+// run button script
+async function runScript(context) {
     const content = context.document.body;
     if (textField.value === "") {
         content.insertParagraph("Who goes there?", "End");
