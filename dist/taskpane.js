872838c5f0d88fefdb918fa30a650d23e268a275
LG-CPFL <lg@clarkpanagakos.com.au>
2024-03-28 11:03:30 +1030

    minor tweaks

--- dist/taskpane.js
+++ dist/taskpane.js
@@ -1,5 +1,13 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // initialise application
 Office.onReady((info) => {
     if (info.host === Office.HostType.Word) {
@@ -37,8 +45,10 @@ function events() {
     });
 }
 // execute script
-async function main(context) {
-    const content = context.document.body;
-    content.insertParagraph("Hello There", "End");
-    await context.sync();
+function main(context) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const content = context.document.body;
+        content.insertParagraph("Hello There", "End");
+        yield context.sync();
+    });
 }
